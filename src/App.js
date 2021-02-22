66f959ab98fe1e066dabe9f7d1b96f54cd3c6632
Yash Mistry <mistryyash100@gmail.com>
2021-02-22 15:22:39 -0400

    build completed with changes fixed

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
   const [id, setId] = useState();
   const [open, setOpen] = useState(false);
 
-
+  console.log(process.env);
 
   useEffect(() => {
-    Axios.get(`${process.env.REACT_APP_API_LINK}/list`).then((res) => {
+    Axios.get('https://yjm-todolist.herokuapp.com/list').then((res) => {
       // const todolist = res.data;
       setTodolist(res.data);
     });
@@ -40,7 +40,7 @@ function App() {
   // CRUD OPERATIONS
 
   const addTask = async () => {
-    await Axios.post(`${process.env.REACT_APP_API_LINK}/insert`, {
+    await Axios.post('https://yjm-todolist.herokuapp.com/insert', {
       task: task,
       isCompleted: isCompleted,
     });
@@ -48,7 +48,7 @@ function App() {
   }
 
   const deteTask = (id) => {
-    Axios.delete(`${process.env.REACT_APP_API_LINK}/delete/${id}`, { data: { id: id } });
+    Axios.delete(`https://yjm-todolist.herokuapp.com/delete/${id}`, { data: { id: id } });
   }
 
   const updateTask = (id) => {
@@ -61,7 +61,7 @@ function App() {
 
   const sendUpdateReq = () => {
     setUpdatedTask(currentTask);
-    Axios.put(`${process.env.REACT_APP_API_LINK}/update`, { id: id, updatedTask: updatedTask });
+    Axios.put("https://yjm-todolist.herokuapp.com/update", { id: id, updatedTask: updatedTask });
     setOpen(false);
   }
 
